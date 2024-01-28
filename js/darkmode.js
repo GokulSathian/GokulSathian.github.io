f6be27a5f741038d19851aa03c5b2c6f573d6967
Gokul <pullarikalgokul@gmail.com>
2024-01-28 13:19:10 -0500

    Updated dark mode with change in profile pic also updated Doc

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,18 @@
 document.getElementById('themeSwitch').addEventListener('change', function(event) {
+    var contactImg = document.getElementById('contact');
+    var aboutImg = document.getElementById('about');
     document.body.classList.toggle('dark-mode', event.target.checked);
     document.body.classList.toggle('light-mode', !event.target.checked);
+    
+  
+    if (event.target.checked) {
+        // Dark mode
+        contactImg.src = "images/black.png";
+        aboutImg.src="images/black.png"
+      } else {
+        // Light mode
+        contactImg.src = "images/profile.png";
+        aboutImg.src="images/profile.png"
+      }
   });
+
